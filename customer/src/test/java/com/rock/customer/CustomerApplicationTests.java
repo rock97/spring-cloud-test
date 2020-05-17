cf3a66bc976cf3f884e027a0c9e4cc24bb1e8143
rock97 <email10010@163.com>
2020-05-17 13:58:14 +0800

    init

---
+++ customer/src/test/java/com/rock/customer/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rock.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/rock/springcloudtest/SpringCloudTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rock.springcloudtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
